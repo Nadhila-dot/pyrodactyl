53725aeaedb9cba670e6d8dbf7de53d7bf5d2579
Nadhi-(Kushi) <udayanthie@nadhi.dev>
2025-08-13 13:07:51 +0530

    update script

---
+++ update.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# nigger root
+if [ "$EUID" -ne 0 ]; then
+  echo "Can't update the panel without root privileges!!!"
+  exit 1
+fi
+
+
+cd "$(dirname "$0")"
+
+
+echo "Syncing with repo"
+git pull origin main || { echo "Failed to pull changes."; exit 1; }
+
+# Build the project using bun
+echo "Building!!!"
+bunx vite build || { echo "Build failed."; exit 1; }
+
+echo "All good, restart webserver"
